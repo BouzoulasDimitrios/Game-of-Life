3cb891023be81d9b633ab6ae30efc3b682daeb16
james <mbouzoulasdimitris@gmail.com>
2023-04-03 22:35:07 +0300

    larger grid, pause & clear buttons

--- main.cpp
+++ main.cpp
@@ -4,27 +4,30 @@
 
 using namespace std;
 
-bool grid[10][10] = {};
+int grid_dim = 90;//20;
+int cell_pos = 10;//25;
+//bool grid[20][20] = {};
+bool grid[90][90] = {};
 
 void edit_board(sf::RenderWindow & window, sf::Event & event,  sf::RectangleShape & cellShape ){
 
     // Determine which cell was clicked
     int mouse_x = event.mouseButton.x , mouse_y = event.mouseButton.y;
 
-    if(( 0 <= mouse_x && mouse_x <= 500) && (0 <= mouse_y && mouse_y <= 500)){ //;
+    if(( 0 <= mouse_x && mouse_x <= 1000) && (0 <= mouse_y && mouse_y <= 900)){ //;
 
-        int x = event.mouseButton.x / 50;
-        int y = event.mouseButton.y / 50;
+        int x = event.mouseButton.x / cell_pos;
+        int y = event.mouseButton.y / cell_pos;
 
         // Toggle the color of the cell;
         grid[x][y] = !grid[x][y];
 
         // Redraw the grid with updated colors
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                cellShape.setPosition(i * 50, j * 50);
+        for (int i = 0; i < grid_dim; i++) {
+            for (int j = 0; j < grid_dim; j++) {
+                cellShape.setPosition(i * cell_pos, j * cell_pos);
                 if (grid[i][j]) {
-                    cellShape.setFillColor(sf::Color::White);
+                    cellShape.setFillColor(sf::Color::Green);
                 } else {
                     cellShape.setFillColor(sf::Color::Black);
                 }
@@ -38,16 +41,16 @@ void edit_board(sf::RenderWindow & window, sf::Event & event,  sf::RectangleShap
 void randomize_board(sf::RenderWindow & window, sf::RectangleShape & cellShape){
 
         // Redraw the grid with updated colors
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < grid_dim; i++) {
+            for (int j = 0; j < grid_dim; j++) {
 
-                cellShape.setPosition(i * 50, j * 50);
+                cellShape.setPosition(i * cell_pos, j * cell_pos);
                 
                 if(rand()%2)
                     grid[i][j] = !grid[i][j];
 
                 if (grid[i][j]) {
-                    cellShape.setFillColor(sf::Color::White);
+                    cellShape.setFillColor(sf::Color::Green);
                 } else {
                     cellShape.setFillColor(sf::Color::Black);
                 }
@@ -64,7 +67,7 @@ bool cout_alive_neighbours(int x, int y){
 
     for(int i = x-1; i <= x+1; i++){
         for(int j = y-1; j <= y+1; j++){
-            if((i == x && j == y) || ((i < 0 || j < 0) || (i > 10 || j > 10)) )
+            if((i == x && j == y) || ((i < 0 || j < 0) || (i > grid_dim || j > grid_dim)) )
                 continue;
             else if(grid[i][j] == 1)    
                 count++;
@@ -72,99 +75,129 @@ bool cout_alive_neighbours(int x, int y){
     }
 
     return grid[x][y] ? (count == 2 || count == 3) : count == 3;
-
 }
 
 
-bool update_board(sf::RenderWindow & window, sf::RectangleShape & cellShape){
+void update_board(sf::RenderWindow & window, sf::RectangleShape & cellShape){
 
-        bool temp_grid[10][10] = {};
+        bool temp_grid[grid_dim][grid_dim] = {};
 
         // Redraw the grid with updated colors
-        for (int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
+        for (int i = 0; i < grid_dim; i++){
+            for(int j = 0; j < grid_dim; j++){
 
-                if(cout_alive_neighbours(i, j)){
-                    // cellShape.setFillColor(sf::Color::White);
-                    temp_grid[i][j] = 1;
-                }else{
-                    temp_grid[i][j] = 0;
-                    // cellShape.setFillColor(sf::Color::Black);
-                }
-
+                cout_alive_neighbours(i,j) ? temp_grid[i][j] = 1 : temp_grid[i][j] = 0; 
                 window.draw(cellShape);
 
             }
         }
 
-        for (int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
+        for (int i = 0; i < grid_dim; i++){
+            for(int j = 0; j < grid_dim; j++){
                 
-                cellShape.setPosition(i * 50, j * 50);
-
-                if(temp_grid[i][j]){
-                    // cout<<"grid = "<<temp_grid[i][j]<<endl;
-                    cellShape.setFillColor(sf::Color::White);
-                    // temp_grid[i][j] = 1;
-                }else{
-                    cellShape.setFillColor(sf::Color::Black);
-                }
+                cellShape.setPosition(i * cell_pos, j * cell_pos);
+                temp_grid[i][j] ? cellShape.setFillColor(sf::Color::Green) : cellShape.setFillColor(sf::Color::Black);
                 window.draw(cellShape);
-
                 grid[i][j] = temp_grid[i][j];
 
             }
         }
 
-        return false;
+
 }
 
 int main()
 {
     bool waiting_to_start = true;
     // Create the window
-    sf::RenderWindow window(sf::VideoMode(500, 600), "Game of life!");
+    sf::RenderWindow window(sf::VideoMode(900, 950), "Game of life!", sf::Style::Close);
 
     // Create a rectangle shape to represent a grid cell
     sf::RectangleShape cellShape(sf::Vector2f(50, 50));
+
     cellShape.setFillColor(sf::Color::Black);
     cellShape.setOutlineThickness(1);
-    cellShape.setOutlineColor(sf::Color::White);
+    cellShape.setOutlineColor(sf::Color::Green);
 
-    sf::Vector2f buttonSize(250.f, 100.f);
-    sf::Vector2f buttonPos1(0.f, 500.f);
-    sf::Vector2f buttonPos2(250.f, 500.f);
+    sf::Vector2f buttonSize(250.f, 50.f);
+    sf::Vector2f buttonPos1(0.f, 900.f);
+    sf::Vector2f buttonPos2(250.f, 900.f);
+    sf::Vector2f buttonPos3(500.f, 900.f);
+    sf::Vector2f buttonPos4(750.f, 900.f);
 
     sf::RectangleShape button1(buttonSize);
-    button1.setPosition(buttonPos1);
     sf::RectangleShape button2(buttonSize);
+    sf::RectangleShape button3(buttonSize);
+    sf::RectangleShape button4(buttonSize);
+
+    button1.setPosition(buttonPos1);
     button2.setPosition(buttonPos2);
+    button3.setPosition(buttonPos3);
+    button4.setPosition(buttonPos4);
 
-    button1.setFillColor(sf::Color::Green);
-    button2.setFillColor(sf::Color::Red);
+    sf::Color button_color(38, 30, 29);
 
-
+    button1.setFillColor(button_color);
+    button2.setFillColor(button_color);
+    button3.setFillColor(button_color);
+    button4.setFillColor(button_color);
+    
     sf::Font font;
     font.loadFromFile("arial.ttf");
 
     sf::Text buttonText1("Start", font);
     buttonText1.setCharacterSize(20);
-    buttonText1.setPosition(buttonPos1.x + 10.f, buttonPos1.y + 10.f);
+    buttonText1.setPosition(buttonPos1.x + 120.f, buttonPos1.y + 10.f);
 
     sf::Text buttonText2("Randomize", font);
     buttonText2.setCharacterSize(20);
     buttonText2.setPosition(buttonPos2.x + 10.f, buttonPos2.y + 10.f);
 
-    buttonText1.setCharacterSize(20);
+    sf::Text buttonText3("Pause", font);
+    buttonText3.setCharacterSize(20);
+    buttonText3.setPosition(buttonPos2.x + 10.f, buttonPos2.y + 10.f);
 
+    sf::Text buttonText4("Pause", font);
+    buttonText4.setCharacterSize(20);
+    buttonText4.setPosition(buttonPos2.x + 10.f, buttonPos2.y + 10.f);
+
+    buttonText1.setCharacterSize(20);
     buttonText2.setCharacterSize(20);
+    buttonText4.setCharacterSize(20);
+    buttonText4.setCharacterSize(20);
+
+    sf::FloatRect textRect1 = buttonText1.getLocalBounds();
+    buttonText1.setOrigin(textRect1.left + textRect1.width/2.0f, textRect1.top  + textRect1.height/2.0f);
+    buttonText1.setPosition(button1.getPosition() + button1.getSize()/2.0f);
+    
+    sf::FloatRect textRect2 = buttonText2.getLocalBounds();
+    buttonText2.setOrigin(textRect2.left + textRect2.width/2.0f, textRect2.top  + textRect2.height/2.0f);
+    buttonText2.setPosition(button2.getPosition() + button2.getSize()/2.0f);
+
+    sf::FloatRect textRect3 = buttonText1.getLocalBounds();
+    buttonText3.setOrigin(textRect1.left + textRect1.width/2.0f, textRect1.top  + textRect1.height/2.0f);
+    buttonText3.setPosition(button1.getPosition() + button1.getSize()/2.0f);
+    
+    sf::FloatRect textRect4 = buttonText2.getLocalBounds();
+    buttonText4.setOrigin(textRect2.left + textRect2.width/2.0f, textRect2.top  + textRect2.height/2.0f);
+    buttonText4.setPosition(button2.getPosition() + button2.getSize()/2.0f);
+
+    button1.setOutlineThickness(1.f);
+    button2.setOutlineThickness(1.f);
+    button3.setOutlineThickness(1.f);
+    button4.setOutlineThickness(1.f);
+    
+    button1.setOutlineColor(sf::Color::Black);
+    button2.setOutlineColor(sf::Color::Black);
+    button3.setOutlineColor(sf::Color::Black);
+    button4.setOutlineColor(sf::Color::Black);
 
     // Draw the grid
-    for (int x = 0; x < 10; x++) {
-        for (int y = 0; y < 10; y++) {
-            cellShape.setPosition(x * 50, y * 50);
+    for (int x = 0; x < grid_dim; x++) {
+        for (int y = 0; y < grid_dim; y++) {
+            cellShape.setPosition(x * cell_pos, y * cell_pos);
             if (grid[x][y]) {
-                cellShape.setFillColor(sf::Color::White);
+                cellShape.setFillColor(sf::Color::Green);
             }
             window.draw(cellShape);
         }
@@ -174,6 +207,9 @@ int main()
     window.draw(buttonText1);
     window.draw(button2);
     window.draw(buttonText2);
+    window.draw(button3);
+    window.draw(button4);
+
 
     // Display the window
     window.display();
@@ -205,6 +241,9 @@ int main()
                     window.draw(buttonText1);
                     window.draw(button2);
                     window.draw(buttonText2);
+                    window.draw(button3);
+                    window.draw(button4);
+
                     window.display();
 
                 }   
@@ -214,8 +253,8 @@ int main()
         if(!waiting_to_start){
             
             update_board(window, cellShape);
-            unsigned int microsecond = 1000000;
-            usleep(1 * microsecond);//sleeps for 3 second
+            unsigned int microsecond = 500000;
+            usleep(1 * microsecond);//sleeps for 1 second
             cout<<"updating\n";
             window.draw(button1);
             window.draw(buttonText1);
